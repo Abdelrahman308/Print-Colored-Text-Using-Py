3164162ea803fb9c4ec0af9456ac0a550b69dc62
Abdelrahman308 <abdo51303@gmail.com>
2021-10-09 08:35:59 +0200

    Print Colored Text

---
+++ Print Colored Text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLACK+Back.RED+"Hi My name is Abdo Mahmoud "+ Fore.BLACK+ Back.WHITE+" I am your Machine Learning Instructor")
+print(Back.LIGHTBLUE_EX+"Hi My name is Abdo Mahmoud ")
+print(Fore.BLACK + Back.RED+ "Hi My name is Abdo Mahmoud ")
